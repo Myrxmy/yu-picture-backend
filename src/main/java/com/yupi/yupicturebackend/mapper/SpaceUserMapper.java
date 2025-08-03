0cdf93a4571f151b8129ac6add7a3d5a212541ec
Myrxmy <1655895179@qq.com>
2025-08-03 09:54:40 +0800

    后端代码
    未脱敏代码

---
+++ src/main/java/com/yupi/yupicturebackend/mapper/SpaceUserMapper.java
@@ -0,0 +1,12 @@
+package com.yupi.yupicturebackend.mapper;
+
+import com.yupi.yupicturebackend.model.entity.SpaceUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @description 针对表【space_user(空间用户关联)】的数据库操作Mapper
+ * @Entity com.yupi.yupicturebackend.model.entity.SpaceUser
+ */
+public interface SpaceUserMapper extends BaseMapper<SpaceUser> {
+
+}
