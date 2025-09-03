aa215ea8ea1b0c1f58ce3439e59f90c68fd9edb8
Myrxmy <1655895179@qq.com>
2025-09-04 01:35:50 +0800

    项目部署上线改动

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,5 +1,7 @@
-create database if not exists `yu_picture` default character set utf8mb4 collate utf8mb4_unicode_ci;
+-- 创建库
+create database if not exists yu_picture;
 
+-- 切换库
 use yu_picture;
 
 -- 用户表
@@ -23,38 +25,29 @@ create table if not exists user
 -- 图片表
 create table if not exists picture
 (
-    id            bigint auto_increment comment 'id' primary key,
-    url           varchar(512)                       not null comment '图片 url',
-    name          varchar(128)                       not null comment '图片名称',
-    introduction  varchar(512)                       null comment '简介',
-    category      varchar(64)                        null comment '分类',
-    tags          varchar(512)                       null comment '标签（JSON 数组）',
-    picSize       bigint                             null comment '图片体积',
-    picWidth      int                                null comment '图片宽度',
-    picHeight     int                                null comment '图片高度',
-    picScale      double                             null comment '图片宽高比例',
-    picFormat     varchar(32)                        null comment '图片格式',
-    reviewStatus  INT      DEFAULT 0                 NOT NULL COMMENT '审核状态：0-待审核; 1-通过; 2-拒绝',
-    reviewMessage VARCHAR(512)                       NULL COMMENT '审核信息',
-    reviewerId    BIGINT                             NULL COMMENT '审核人 ID',
-    reviewTime    DATETIME                           NULL COMMENT '审核时间',
-    userId        bigint                             not null comment '创建用户 id',
-    createTime    datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    editTime      datetime default CURRENT_TIMESTAMP not null comment '编辑时间',
-    updateTime    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete      tinyint  default 0                 not null comment '是否删除',
+    id           bigint auto_increment comment 'id' primary key,
+    url          varchar(512)                       not null comment '图片 url',
+    name         varchar(128)                       not null comment '图片名称',
+    introduction varchar(512)                       null comment '简介',
+    category     varchar(64)                        null comment '分类',
+    tags         varchar(512)                       null comment '标签（JSON 数组）',
+    picSize      bigint                             null comment '图片体积',
+    picWidth     int                                null comment '图片宽度',
+    picHeight    int                                null comment '图片高度',
+    picScale     double                             null comment '图片宽高比例',
+    picFormat    varchar(32)                        null comment '图片格式',
+    userId       bigint                             not null comment '创建用户 id',
+    createTime   datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    editTime     datetime default CURRENT_TIMESTAMP not null comment '编辑时间',
+    updateTime   datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete     tinyint  default 0                 not null comment '是否删除',
     INDEX idx_name (name),                 -- 提升基于图片名称的查询性能
     INDEX idx_introduction (introduction), -- 用于模糊搜索图片简介
     INDEX idx_category (category),         -- 提升基于分类的查询性能
     INDEX idx_tags (tags),                 -- 提升基于标签的查询性能
-    INDEX idx_userId (userId),             -- 提升基于用户 ID 的查询性能
-    INDEX idx_reviewStatus (reviewStatus)  -- 创建基于 reviewStatus 列的索引
+    INDEX idx_userId (userId)              -- 提升基于用户 ID 的查询性能
 ) comment '图片' collate = utf8mb4_unicode_ci;
 
-
-
-
-use yu_picture;
 
 ALTER TABLE picture
     -- 添加新列
@@ -69,6 +62,7 @@ CREATE INDEX idx_reviewStatus ON picture (reviewStatus);
 ALTER TABLE picture
     -- 添加新列
     ADD COLUMN thumbnailUrl varchar(512) NULL COMMENT '缩略图 url';
+
 
 -- 空间表
 create table if not exists space
@@ -93,7 +87,7 @@ create table if not exists space
 
 -- 添加新列
 ALTER TABLE picture
-    ADD COLUMN spaceId  bigint  null comment '空间 id（为空表示公共空间）';
+    ADD COLUMN spaceId bigint  null comment '空间 id（为空表示公共空间）';
 
 -- 创建索引
 CREATE INDEX idx_spaceId ON picture (spaceId);
@@ -123,3 +117,8 @@ create table if not exists space_user
     INDEX idx_userId (userId)                       -- 提升按用户查询的性能
 ) comment '空间用户关联' collate = utf8mb4_unicode_ci;
 
+-- 扩展用户表：新增会员功能
+ALTER TABLE user
+    ADD COLUMN vipExpireTime datetime NULL COMMENT '会员过期时间',
+    ADD COLUMN vipCode varchar(128) NULL COMMENT '会员兑换码',
+    ADD COLUMN vipNumber bigint NULL COMMENT '会员编号';
